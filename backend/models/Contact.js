c653a92278ce659f3eedc99b318b5ecca1ce2ddc
akash <isratjahanarna@gmail.com>
2026-04-04 23:16:02 +0600

    Backend: auth and contact fix

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   name: { type: String, required: true },
   email: { type: String, required: true },
   subject: { type: String, required: true },
